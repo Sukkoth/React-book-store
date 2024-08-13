18ed178c7fd75f85afe6203e4872c0f135c58acf
Sukkoth <suukootj@gmail.com>
2024-08-14 00:00:07 +0300

    little one

--- src/pages/UI/Details.tsx
+++ src/pages/UI/Details.tsx
@@ -6,6 +6,9 @@ function Details() {
   return (
     <main className='text-sm ui-font'>
       <Nav />
+      <div className='px-5 max-w-[1325px] mx-auto mt-10'>
+        <div className='w-[40rem] aspect-square bg-stone-100'></div>
+      </div>
     </main>
   );
 }
